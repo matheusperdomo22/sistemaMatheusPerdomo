e2301f9ce08652047fea19ec037c118a7b3d2709
matheusperdomo22 <matheus.perdomo2@estudante.ifms.edu.br>
2025-10-14 00:08:51 -0400

    Fiz o hibernet e criei parte dos DAOS

---
+++ src/dao/Mcp_ClientesDAO.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import bean.McpClientes;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
+
+
+/**
+ *
+ * @author u1845853
+ */
+public class Mcp_ClientesDAO extends AbstractDAO{
+
+    @Override
+    public void insert(Object object) {
+        session.beginTransaction();
+        session.save(object);
+        session.getTransaction().commit();        
+    }
+
+    @Override
+    public void update(Object object) {
+        session.beginTransaction();
+        session.flush();
+        session.clear();
+        session.update(object);
+        session.getTransaction().commit();        
+    }
+
+    @Override
+    public void delete(Object object) {
+        session.beginTransaction();
+        session.flush();
+        session.clear();        
+        session.delete(object);
+        session.getTransaction().commit();        
+    }
+
+    @Override
+    public Object list(int codigo) {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(McpClientes.class);
+        criteria.add(Restrictions.eq("idclientes", codigo));
+        List lista = criteria.list();
+        session.getTransaction().commit();        
+        return lista;
+    }
+
+    @Override
+    public Object listAll() {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(McpClientes.class);
+        List lista = criteria.list();
+        session.getTransaction().commit();        
+        return lista;    
+    }
+
+    public static void main(String[] args) {
+        Mcp_ClientesDAO clientesDAO = new Mcp_ClientesDAO();
+        clientesDAO.listAll();
+    }
+}
